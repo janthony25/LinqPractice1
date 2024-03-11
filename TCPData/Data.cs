16c7205976a9aa5c9207fa803a487a2331182f42
Jun Anthony Calupcupan <junanthony.business@gmail.com>
2024-03-11 18:41:09 +1300

    Add project files.

---
+++ TCPData/Data.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TCPData
+{
+    public static class Data
+    {
+        public static List<Employee> GetEmployees()
+        {
+            List<Employee> employees = new List<Employee>();
+
+            Employee employee = new Employee
+            {
+                Id = 1,
+                FirstName = "Bob",
+                LastName = "Jones",
+                AnnualSalary = 60000.3m,
+                IsManager = true,
+                DepartmentId = 1,
+
+            };
+            employees.Add(employee);
+
+            employee = new Employee
+            {
+                Id = 1,
+                FirstName = "Sarah",
+                LastName = "Jameson",
+                AnnualSalary = 80000.3m,
+                IsManager = true,
+                DepartmentId = 2,
+            };
+            employees.Add(employee );
+
+            employee = new Employee
+            {
+                Id = 3,
+                FirstName = "Douglas",
+                LastName = "Roberts",
+                AnnualSalary = 40000.3m,
+                IsManager = false,
+                DepartmentId = 3,
+            };
+            employees.Add(employee);
+
+            employee = new Employee
+            {
+                Id = 1,
+                FirstName = "Jane",
+                LastName = "Stevens",
+                AnnualSalary = 30000.2m,
+                IsManager = false,
+                DepartmentId = 4,
+            };
+            employees.Add(employee);
+
+            return employees;
+
+            
+        }
+
+        public static GetDepartments()
+        {
+            List<Department> departments = new List<Department>();
+
+            Department department = new Department
+            {
+                Id = 1,
+                ShortName = "HR",
+                LongName = "Human Resources"
+            };
+
+            department = new Department
+            {
+                Id = 2,
+                ShortName = "FN",
+                LongName = "Finance"
+            };
+
+            department = new Department
+            {
+                Id = 3,
+                ShortName = "TE",
+                LongName = "Technology"
+            };
+        }
+    }
+}
